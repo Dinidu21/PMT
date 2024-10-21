aab550424a5e45f5a403dba2f32469d9e5e0e376
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-10-22 00:08:17 +0530

    moved to dashboard & initialize 12.05 A.M. 2024-10-22

--- src/main/java/com/dinidu/lk/pmt/controller/dashboard/DashboardExpandViewController.java
+++ src/main/java/com/dinidu/lk/pmt/controller/dashboard/DashboardExpandViewController.java
@@ -1,4 +1,22 @@
 package com.dinidu.lk.pmt.controller.dashboard;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class DashboardExpandViewController {
+
+    @FXML
+    private ComboBox<String> dashboardComboBox; // Define ComboBox with fx:id
+
+    public void initialize() {
+        // Set the initial selection
+        dashboardComboBox.setValue("Business Overview");
+
+        // Add items to the ComboBox
+        dashboardComboBox.getItems().addAll(
+                "Business Overview",
+                "My Dashboard",
+                "Webteam Dashboard"
+        );
+    }
 }
